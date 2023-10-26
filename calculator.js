68a15804d0ee260a33cf7f4d38d736507b8f4bf6
20 <110159240+20CenturyBoy@users.noreply.github.com>
2023-10-26 18:11:22 +0200

    Defines all necessary calculator processes together with interface and connects the two
    All of essential calculation processes are implemented and made intractable
    with via web page interface via overall handler function together with
    defineExpression and evaluateExpression functions.
    First overall handler function in order to react to user input is tasked
    with interpreting it and calling one of two appropriate functions due to
    each user interaction being of one of two types and each function being
    tasked with handling one of them and is tasked with handling all things
    necessary to enable appropriate interaction between two functions.
    defineExpression function makes sure that each user input that isnt
    request for evaluation of expression is properly recorded as part of
    expression and also exposed/displayed as part of user interface .
    While evaluateExpression is tasked with parsing expression and passing it
    to calculate function in order to fully evaluate it and return result.

--- calculator.js
+++ calculator.js
@@ -14,13 +14,68 @@ function divide(a,b){
     return a / b;
 }
 
-
 function calculate(operator, a, b){
- if(operator == '+') add(a,b);
- if(operator == '-') subtract(a,b);
- if(operator == '*') multiply(a,b);
- if(operator == '/') divide(a,b);  
+ if(operator == '+') return add(a,b);
+ if(operator == '-') return subtract(a,b);
+ if(operator == '*') return multiply(a,b);
+ if(operator == '/') return divide(a,b);  
 }
+
+function updateExpression(button)
+{
+   if (expressionEvaluated == true){
+    expression = "";
+   }
+   if(operators.includes(expression[expression.length-1]) && operators.includes(button)){
+        expression = expression.slice(0,expression.length-1) + button;
+        display.innerText = expression;
+   }
+   else{
+    expression = expression + button;
+    display.innerText = expression;
+   }
+
+}
+
+function evaluateExpression(operator,expression){
+   let operands = expression.split(operator);
+   return calculate(operator,Number(operands[0]),Number(operands[1]));
+}
+
+
+let buttons = document.querySelectorAll('button');
+let operators = ['+','-','*','/'];
+let display = document.querySelector('#display');
+let expression = display.innerText;
+let expressionEvaluated = false;
+let operator = undefined;
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let buttonClicked = button.innerText;
+        if ((operators.includes(buttonClicked )|| buttonClicked == "=") && (!operators.includes(expression[expression.length-1])) && (expression.includes('+') || expression.includes('-')|| expression.includes('*')|| expression.includes("/"))){
+            let result = evaluateExpression(operator,expression);
+            expressionEvaluated = true;
+            updateExpression(result);
+            expressionEvaluated = false;
+            if(!(buttonClicked == '=')){
+                updateExpression(buttonClicked);  
+            }
+        }
+        else if (buttonClicked == "<-")
+        {
+            expression = "";
+            updateExpression(expression);
+        }
+        else{
+            if(!(buttonClicked == '=')){
+                updateExpression(buttonClicked);
+            }     
+        }
+        if (operators.includes(buttonClicked)){
+             operator = buttonClicked;
+        }
+    });
+});
 
 
 
